249e4ace3ae6d03d873b43eb49ff7bf613cfbe5a
Mariya <mariasherbakiva@mail.ru>
2024-09-15 13:53:33 +0600

    task 1

---
+++ hello.js
@@ -0,0 +1,15 @@
+// Получаем элементы по их id
+const string3 = document.getElementById('string-3').textContent;
+const string5 = document.getElementById('string-5').textContent;
+const string2 = document.getElementById('string-2').textContent;
+const string6 = document.getElementById('string-6').textContent;
+const string4 = document.getElementById('string-4').textContent;
+const string1 = document.getElementById('string-1').textContent;
+
+// Выводим содержимое в консоль в нужном порядке
+console.log(string3);
+console.log(string5);
+console.log(string2);
+console.log(string6);
+console.log(string4);
+console.log(string1);
